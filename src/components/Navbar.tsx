04fcba4a50d1b53d11a877ac5ef5e811a97aba3f
c23mj <mjjiang@umich.edu>
2025-03-06 11:35:06 -0800

    more mobile fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar: FC = () => {
     <nav className="w-full flex justify-center py-8">
       <div className="flex flex-col w-full">
         <div className="w-full flex text-xl mb-6">
-          <div className="w-1/2 md:w-3/5 pl-8">
+          <div className="navbar-left-column pl-8">
             <a className="" href="/">Home</a>
           </div>
-          <div className="w-1/2 md:w-2/5 flex justify-between">
+          <div className="navbar-right-column flex justify-between">
             <a className="" href="/work">Work</a>
             <a className="" href="/projects">Projects</a>
             <a className="" href="/misc">Misc.</a>
